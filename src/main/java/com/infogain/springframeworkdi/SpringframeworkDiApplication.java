83f4ace0dfcc26506fe356a5d80e4b5552273073
khushboojain93 <khushboojainmca@gmail.com>
2020-08-12 01:06:41 +0530

    implemented all types of spring di

--- src/main/java/com/infogain/springframeworkdi/SpringframeworkDiApplication.java
+++ src/main/java/com/infogain/springframeworkdi/SpringframeworkDiApplication.java
@@ -1,13 +1,36 @@
 package com.infogain.springframeworkdi;
 
+import com.infogain.springframeworkdi.controller.ConstructorInjectedController;
+import com.infogain.springframeworkdi.controller.PropertyInjectedController;
+import com.infogain.springframeworkdi.controller.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringframeworkDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringframeworkDiApplication.class, args);
+		ApplicationContext ctx=SpringApplication.run(SpringframeworkDiApplication.class, args);
+
+		PropertyInjectedController propertyInjectedController=(PropertyInjectedController)ctx.getBean("propertyInjectedController");
+
+		System.out.println("----------Property Based Injection--------");
+
+		System.out.println(propertyInjectedController.getGreeting());
+
+		SetterInjectedController setterInjectedController=(SetterInjectedController)ctx.getBean("setterInjectedController");
+
+		System.out.println("--------------Setter Based Injection-----------");
+
+		System.out.println(setterInjectedController.getGreeting());
+
+		ConstructorInjectedController constructorInjectedController=(ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+
+		System.out.println("------------Constructor Based Injection-----------");
+
+		System.out.println(constructorInjectedController.getGreeting());
+
 	}
 
 }
---
+++ src/main/java/com/infogain/springframeworkdi/service/GreetingService.java
@@ -0,0 +1,8 @@
+package com.infogain.springframeworkdi.service;
+
+import org.springframework.stereotype.Service;
+
+public interface GreetingService {
+
+    String sayGreeting();
+}
