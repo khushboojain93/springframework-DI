dfa8dc709a65050d8df2d0bbcb4e698727df61c1
khushboojain93 <khushboojainmca@gmail.com>
2020-08-11 22:52:48 +0530

    Initial commit

---
+++ src/test/java/com/infogain/springframeworkdi/SpringframeworkDiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.infogain.springframeworkdi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringframeworkDiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
